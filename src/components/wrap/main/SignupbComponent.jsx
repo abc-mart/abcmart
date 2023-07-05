ed6d178abe3d5f4a6b3a73fddba6154ed80a65db
shurubin <susubini03@naver.com>
2023-07-05 21:44:14 +0900

    subin0705

--- src/components/wrap/main/SignupbComponent.jsx
+++ src/components/wrap/main/SignupbComponent.jsx
@@ -14,7 +14,7 @@ export default function SignupbComponent(){
                             <div className="substance">
                                 <form action="" method='post'>
                                     <div className="box1">
-                                        <div className="box11">약관동의</div>
+                                        <div className="box11"><h4>약관동의</h4></div>
                                         <div className="box12">
                                             <ul>
                                                 <li><input type="checkbox"/>전체 약관에 동의합니다.</li>
@@ -26,15 +26,15 @@ export default function SignupbComponent(){
                                         </div>
                                     </div>
                                     <div className="box2">
-                                        <div className="box21">회원정보</div>
+                                        <div className="box21"><h4>회원정보</h4></div>
                                         <div className="box22">
                                             <ul>
-                                                <li><div className="colTitle"></div><input type="text" name='nm'/></li>
-                                                <li><div className="colTitle"></div><input type="text" name='id'/></li>
-                                                <li><div className="colTitle"></div><input type="text" name='pw'/></li>
-                                                <li><div className="colTitle"></div><input type="text" name='pc'/></li>
-                                                <li><div className="colTitle"></div><input type="text" name='em'/></li>
-                                                <li><div className="colTitle"></div><input type="text" name='ph'/></li>
+                                                <li><div className="colTitle">이름<i></i></div><input type="text" name='nm' placeholder='한글, 영문, 숫자만 입력해주세요'/></li>
+                                                <li><div className="colTitle">아이디<i></i></div><input type="text" name='id' placeholder='아이디를 입력해주세요 (영문, 숫자 사용 3~20자)'/></li>
+                                                <li><div className="colTitle">비밀번호<i></i></div><input type="text" name='pw' placeholder='비밀번호를 입력해 주세요. (영문, 숫자, 특수문자 2개 이상 10~20자)'/></li>
+                                                <li><div className="colTitle">비밀번호 확인<i></i></div><input type="text" name='pc' placeholder='비밀번호를 재입력해 주세요.'/></li>
+                                                <li><div className="colTitle">이메일<i></i></div><input type="text" name='em' placeholder='이메일 주소를 입력해 주세요.'/></li>
+                                                <li><div className="colTitle">휴대폰 번호</div><input type="text" name='ph' placeholder='휴대폰 번호를 ‘-’ 없이 입력해주세요.'/></li>
                                             </ul>
                                         </div>
                                     </div>
@@ -42,7 +42,10 @@ export default function SignupbComponent(){
 
                                     </div>
                                     <div className="box4">
-                                        <button type='submit'>회원가입</button>
+                                        <div className="btn-box">
+                                            <button>취소</button>
+                                            <button type='submit'>회원가입</button>
+                                        </div>
                                     </div>
                                 </form>
                             </div>
