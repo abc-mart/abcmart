a4d2a453fb9f59acce918f8efca07009e0463c65
shurubin <susubini03@naver.com>
2023-07-12 18:04:09 +0900

    subin0712

--- src/components/wrap/main/LoginComponent.jsx
+++ src/components/wrap/main/LoginComponent.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 import './scss/login.scss';
 
 export default function LoginComponent(){
+
+    const [member, setMember] = React.useState(false);
+
+    const onClickMember=(e)=>{
+        e.preventDefault();
+        setMember(true);
+    }
+
+    const onClickNotMember=(e)=>{
+        e.preventDefault();
+        setMember(false);
+    }
+
     return (
         <div id='login'>
             <div id="wrap">
@@ -14,24 +27,30 @@ export default function LoginComponent(){
                             <div className="substance">
                                 <div className="box1">
                                     <div className="box11">
-                                        <p>회원 로그인</p>
+                                        <a href="!#" onClick={onClickMember} className={member?'on':''}>회원 로그인</a>
+                                        <a href="!#" onClick={onClickNotMember} className={member?'':'on'}>비회원 주문조회</a>
                                     </div>
-                                    <div className="box12">
-                                        <p>비회원 주문조회</p>
-                                    </div>
+
                                 </div>
                                 <div className="box2">
                                     <form action="#" method='POST'>
                                         <div className='box21'>
                                             <div className='box211'><input type="text" name='id' placeholder='아이디를 입력해 주세요' /></div>
                                             <div className='box212'><input type="text" name='pw' placeholder='비밀번호를 입력해 주세요' /></div>
-                                            <div className="box213">
-									            <input className="checkboxSaveID" type="checkbox"/>
-									            <label for="chkSaveID">아이디 저장</label>
+                                            <div className="box213">	
+                                                <input id='saveId' type="checkbox" />	
+                                                <label htmlFor="saveId">아이디 저장</label>							                                                                
 								            </div>
                                         </div>
                                         <div className='box22'>
-
+                                            <button type='submit'>로그인</button>
+                                        </div>
+                                        <div className="box23">
+                                            <a href="!#"> 아이디 찾기</a>
+                                            <i>·</i>
+                                            <a href="!#">비밀번호 찾기</a>
+                                            <i>·</i>
+                                            <a href="!#">회원가입</a>
                                         </div>
                                     </form>
                                 </div>
